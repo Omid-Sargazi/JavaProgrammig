66c296b6292bf55660ff531b112cd0528565c28b
Omid-Sargazi <om.sargazi@gmail.com>
2024-09-17 14:25:28 -0400

    create a class for identify seniority

---
+++ JavaProgramming/SRP/WithSRP/SeniorityChecker.java
@@ -0,0 +1,8 @@
+package JavaProgramming.SRP.WithSRP;
+
+public class SeniorityChecker {
+    public String checkSeniority(double  experience)
+    {
+        return  experience>5?"Senior":"Junior";
+    }
+}
