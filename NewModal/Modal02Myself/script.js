874255392268cbd9f7b8aa4f509ec8e8028f3936
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-01 01:47:40 +0330

    getting all var from html file

---
+++ NewModal/Modal02Myself/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let selectedProducts = [];
+    const MAX_SELECTION = 4;
+
+    const modal = document.getElementById("modal");
+    const modalClose = document.getElementsByClassName("close")[0];
+    const choiceItemBtn = document.getElementById("choiceItemBtn");
+    const selectedProductsContainer = document.getElementById("selectedProducts");
+    const productList = document.getElementById("productList");
+
+    
+})
