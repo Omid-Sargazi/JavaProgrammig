54f55ef62e4a706aa31278e88138080750d92f35
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-01 01:49:12 +0330

    creaet json file

--- NewModal/Modal02Myself/script.js
+++ NewModal/Modal02Myself/script.js
@@ -8,5 +8,13 @@ document.addEventListener("DOMContentLoaded",()=>{
     const selectedProductsContainer = document.getElementById("selectedProducts");
     const productList = document.getElementById("productList");
 
-    
+    const products = [
+        { id: 1, name: "Product 1", price: "$50", image: "./Image/1.png", description: "Product 1 description" },
+        { id: 2, name: "Product 2", price: "$60", image: "./Image/2.png", description: "Product 2 description" },
+        { id: 3, name: "Product 3", price: "$70", image: "./Image/3.png", description: "Product 3 description" },
+        { id: 4, name: "Product 4", price: "$80", image: "./Image/1.png", description: "Product 4 description" },
+        { id: 5, name: "Product 5", price: "$90", image: "./Image/2.png", description: "Product 5 description" }
+    ];
+
+
 })
