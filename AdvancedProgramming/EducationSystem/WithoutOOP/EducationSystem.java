c3bf0e94ab0240e665b18a7547d9e23d176baa63
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-02 23:59:16 +0330

    insert course, grade and student without OOP

--- AdvancedProgramming/EducationSystem/WithoutOOP/EducationSystem.java
+++ AdvancedProgramming/EducationSystem/WithoutOOP/EducationSystem.java
@@ -6,10 +6,20 @@ public class EducationSystem {
 
     static  ArrayList<String> students = new ArrayList<>();
     static  ArrayList<String> courses = new ArrayList<>();
-    static  ArrayList<String> grades = new ArrayList<>();
+    static  ArrayList<Integer> grades = new ArrayList<>();
 
      public static void main(String[] args) {
         System.out.println("hello");
+
+        addStudent("John Doe");
+        addStudent("omid Sa");
+
+        addCourse("Math");
+        addCourse("Pro");
+
+        assignGrade(0, 0, 85);
+        assignGrade(1, 1, 90);
+        printStudent(0);
 
      }
 
@@ -21,5 +31,17 @@ public class EducationSystem {
      public static void addCourse(String course)
      {
         courses.add(course);
+     }
+
+     public static void assignGrade(int studentIndex, int courseIndex, int grade)
+     {
+        grades.add(grade);
+     }
+
+     public static void printStudent(int index)
+     {
+        System.out.println("Student: " + students.get(index));
+        System.out.println("Course: " + courses.get(index));
+        System.out.println("Grades: " + grades.get(index));
      }
 }
